0c6f22a59ba1c29b18eca572a44418e79f04f629
egrils <angela335889390@gmail.com>
2018-04-03 15:41:19 +0800

    add sellItem_component

--- public/js/components/App.js
+++ public/js/components/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import TopBar from './TopBar';
 import ShowItems from './ShowItems';
+import SellItem from './SellItem';
 
 class App extends Component {
   render() {
     return <div>
       <TopBar/>
-      <ShowItems/>
+      <SellItem/>
     </div>;
   }
 }
---
+++ public/js/components/SellItem.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+export default class SellItem extends Component {
+
+  getImg() {
+    let files = this.imgFile.files;
+
+    if (files) {
+      [].forEach.call(files, readAndPreview.bind(this));
+    }
+
+    function readAndPreview(file) {
+
+      // Make sure `file.name` matches our extensions criteria
+      if (/\.(jpe?g|png|gif)$/i.test(file.name)) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.addEventListener("load", ()=> {
+          let image = new Image();
+          image.height = 100;
+          image.title = file.name;
+          image.src = reader.result;
+          this.preview.appendChild(image);
+
+          let imgdataUrl = reader.result;
+          this.props.getImgData(imgdataUrl);
+        }, false);
+      }
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        选择图片：
+        <input type="file" id="imgFile" name="file" ref={(c) => this.imgFile = c}
+               onChange={this.getImg.bind(this)}/>
+        <div id="preview" ref={(c) => this.preview = c}></div>
+      </div>
+    );
+  }
+}
