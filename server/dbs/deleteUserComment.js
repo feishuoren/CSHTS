79c8b1195508ef8d4caf7b74fa9c3e37b0f19a55
egrils <angela335889390@gmail.com>
2018-05-05 23:02:08 +0800

    fix delete comment when the item has deleted

--- server/dbs/deleteUserComment.js
+++ server/dbs/deleteUserComment.js
@@ -10,7 +10,7 @@ function deleteUserComment(req, callback) {
   const theItemComment = {nickname, sno, comment, addTime};
   const theUserComment = req.body.theUserComment;
 
-  cn.MongoClient.connect(cn.url, (err, database)=> {
+  cn.MongoClient.connect(cn.url, (err, database) => {
     if (err) {
       console.log(err);
       throw err;
@@ -20,12 +20,14 @@ function deleteUserComment(req, callback) {
       const userCollection = database.collection('users');
 
       itemCollection.findOne({_id: ObjectID(itemId)}, (err, result) => {
-        result.itemComments.splice(result.itemComments.indexOf(theItemComment),1);
-        itemCollection.save(result);
+        if (result) {
+          result.itemComments.splice(result.itemComments.indexOf(theItemComment), 1);
+          itemCollection.save(result);
+        }
       });
 
       userCollection.findOne({sno: sno}, (err, result) => {
-        result.myComments.splice(result.myComments.indexOf(theUserComment),1);
+        result.myComments.splice(result.myComments.indexOf(theUserComment), 1);
         userCollection.save(result);
         callback(result.myComments);
       });
