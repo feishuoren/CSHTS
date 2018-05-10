42b157dd288f9fb51b352dbedc3a7223788c32bf
egrils <angela335889390@gmail.com>
2018-05-10 19:56:52 +0800

    refactor ItemMessage

--- public/js/components/ItemMessage.js
+++ public/js/components/ItemMessage.js
@@ -7,8 +7,45 @@ export default class ItemMessage extends Component {
     this.props.getItemMessage(itemId);
   }
 
+  render() {
+    return (
+      <div>
+        <ItemMesBox itemMessage={this.props.itemMessage}/>
+        <ItemComments itemMessage={this.props.itemMessage} addComment={this.props.addComment}/>
+      </div>
+    );
+  }
+}
+
+class ItemMesBox extends Component {
+  render() {
+    const itemPicture = this.props.itemMessage.itemPicture;
+    const itemName = this.props.itemMessage.itemName;
+    const itemBrand = this.props.itemMessage.itemBrand;
+    const itemPrice = this.props.itemMessage.itemPrice;
+    const contactInfo = this.props.itemMessage.contactInfo;
+    const itemSynopsis = this.props.itemMessage.itemSynopsis;
+
+    return (
+      <div id="itemMesBox">
+        <div id="itemMesPic">
+          <img src={itemPicture}/>
+        </div>
+        <div>名称:{itemName}</div>
+        <div>厂牌:{itemBrand}</div>
+        <div>价格:{itemPrice}元</div>
+        <div>联系方式：{contactInfo}</div>
+        <div>简介:{itemSynopsis}</div>
+      </div>
+    );
+  }
+}
+
+class ItemComments extends Component {
   setCommentList() {
-    return this.props.itemMessage.itemComments.reverse().map((ele, index)=> {
+
+    return this.props.itemMessage.itemComments.reverse().map((ele, index) => {
+
       return <div key={index} className="commentBox">
         <div>
           {ele.comment}
@@ -34,6 +71,7 @@ export default class ItemMessage extends Component {
         const nickname = this.getCookieUser().nickname;
         const sno = this.getCookieUser().sno;
         const comment = this.inputComment.value;
+
         this.props.addComment(itemId, itemPicture, itemName, nickname, sno, comment);
       }
     }
@@ -55,35 +93,18 @@ export default class ItemMessage extends Component {
         sno = cookieValue;
       }
     });
+
     return {nickname, sno};
   }
 
   render() {
-    const itemPicture = this.props.itemMessage.itemPicture;
-    const itemName = this.props.itemMessage.itemName;
-    const itemBrand = this.props.itemMessage.itemBrand;
-    const itemPrice = this.props.itemMessage.itemPrice;
-    const contactInfo = this.props.itemMessage.contactInfo;
-    const itemSynopsis = this.props.itemMessage.itemSynopsis;
     const itemComments = Array.isArray(this.props.itemMessage.itemComments) && this.props.itemMessage.itemComments.length > 0 ? this.setCommentList() : '暂时没有评论，等待你来发掘';
 
     return (
       <div>
-        <div id="itemMesBox">
-          <div id="itemMesPic">
-            <img src={itemPicture}/>
-          </div>
-          <div>名称:{itemName}</div>
-          <div>厂牌:{itemBrand}</div>
-          <div>价格:{itemPrice}元</div>
-          <div>联系方式：{contactInfo}</div>
-          <div>简介:{itemSynopsis}</div>
-        </div>
-        <div>
-          <input type="text" ref={(c)=>this.inputComment = c}/>
-          <button onClick={this.submitComment.bind(this)}>我要评论</button>
-          {itemComments}
-        </div>
+        <input type="text" ref={(c) => this.inputComment = c}/>
+        <button onClick={this.submitComment.bind(this)}>我要评论</button>
+        {itemComments}
       </div>
     );
   }
